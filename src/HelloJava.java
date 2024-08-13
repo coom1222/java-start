61ee63b42ac6dbad0bb2c044fb1d8a00198196d1
junmin <coom1222@gmail.com>
2024-08-13 13:49:38 +0900

    ~ 자바 프로그램 실행까지

---
+++ src/HelloJava.java
@@ -0,0 +1,17 @@
+public class HelloJava {
+
+    // 시작을 하면 자바는 코드의 시작지점을 찾고, main을 만나 실행한다.
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }   // main 매서드의 범위끝
+}   // 클래스의 범위 끝
+
+// 자바 언어는 대소문자를 구분한다. 유의해서 작성하자.
+
+/*
+*  public class HelloJava -> HelloJava를 클래스라 한다. 클래스의 개념을 학습해야 할 수 있다. 클래스는 뒤에서 학습한다.
+*  public static void 는 매서드라 한다. 이걸 만나서 프로그램의 시작점이 된다.
+*  String[] -> 배열.
+*  System.out.println -> 콘솔에 출력. 자바는 문자열을 쓸 때 쌍따옴표를 사용한다.
+*
+* */
